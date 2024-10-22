caa03bb171e687bbe6064d55a0e05c8db7de7773
lathiesh24 <lathumahi24@gmail.com>
2024-10-22 15:15:53 +0530

    Aesthetic changes

--- src/app/components/TrendsWeb/EcosystemWeb.tsx
+++ src/app/components/TrendsWeb/EcosystemWeb.tsx
@@ -27,52 +27,50 @@ const EcosystemWeb = ({ handleExploreClick, selectedEcosystem, handleClose }) =>
     enhancement,
     measureOfImpact: measurementOfImpact,
     startups,
-  } = useCaseData || {}; // Destructure use case data if found, else empty
+  } = useCaseData || {};
 
   return (
-    <div className="mx-auto w-[400px] h-[83vh] flex mt-8">
-      <div className="flex flex-col gap-8 justify-around items-center bg-white shadow-lg px-5 rounded py-8 overflow-y-auto w-full h-full scrollbar-thin">
-        <div className="relative flex flex-col gap-8 py-6 px-4 w-full bg-blue-400">
-          <div
-            className="text-white font-semibold absolute right-1 top-1 cursor-pointer z-10"
-            onClick={handleClose}
-          >
-            <IoClose size={23} color="white" />
-          </div>
-          <img
-            src="ecosystembg.png"
-            alt="Ecosystem Background"
-            className="absolute inset-0 w-full h-full object-cover opacity-50 z-0"
-          />
+    <div className="flex flex-col gap-4 items-center bg-white shadow-lg  rounded overflow-y-auto  pb-4 scrollbar-thin m-auto w-[400px] h-[85vh]">
+      <div className="relative flex flex-col gap-8 py-6 px-4 w-full bg-blue-400">
+        <div
+          className="text-white font-semibold absolute right-1 top-1 cursor-pointer z-10"
+          onClick={handleClose}
+        >
+          <IoClose size={23} color="white" />
+        </div>
+        <img
+          src="ecosystembg.png"
+          alt="Ecosystem Background"
+          className="absolute inset-0 w-full h-full object-cover opacity-50 z-0"
+        />
 
-          <div className="relative z-10 text-center text-white font-medium text-xl">
-            {useCase || "Ecosystem not found"}
-          </div>
-
-          <div
-            className="relative z-10 flex mx-auto px-8 py-3 rounded-md font-medium bg-blue-200 text-black border-[1px] w-max justify-center items-center cursor-pointer"
-            onClick={handleExploreClick}
-          >
-            Explore Ecosystem
-          </div>
+        <div className="relative z-10 text-center text-white font-medium text-xl">
+          {useCase || "Ecosystem not found"}
         </div>
 
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-lg">Description</div>
-            <div className="text-base">
-              {description || "No description available."}
-            </div>
+        <div
+          className="relative z-50 text-sm font-medium  mx-auto px-4 py-2 cursor-pointer rounded-md shadow-md bg-blue-200 text-black"
+          onClick={handleExploreClick}
+        >
+          Explore Ecosystem
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 px-5">
+        <div className="flex flex-col gap-2">
+          <div className="font-semibold text-lg">Description</div>
+          <div className="text-base">
+            {description || "No description available."}
           </div>
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-lg">Enhancement</div>
-            <div>{enhancement || "No enhancement information available."}</div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="font-semibold text-lg">Measurement of Impact</div>
-            <div>
-              {measurementOfImpact || "No measurement of impact available."}
-            </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="font-semibold text-lg">Enhancement</div>
+          <div>{enhancement || "No enhancement information available."}</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="font-semibold text-lg">Measurement of Impact</div>
+          <div>
+            {measurementOfImpact || "No measurement of impact available."}
           </div>
         </div>
       </div>
--- src/app/components/TrendsWeb/StartupsWeb.tsx
+++ src/app/components/TrendsWeb/StartupsWeb.tsx
@@ -25,61 +25,65 @@ const StartupsWeb = ({ handleEcosystem, selectedEcosystem , handleExploreClick ,
   const { startups } = useCaseData || {};
 
   return (
-    <div className="mx-auto w-[400px] flex justify-center items-center h-screen">
-      <div className="flex flex-col gap-8 justify-center items-center bg-white">
-        <div className="relative flex flex-col justify-center items-center gap-8 mx-4 bg-blue-400 h-[200px] overflow-hidden">
-          <div className="text-white font-semibold absolute right-1 top-1 cursor-pointer z-10" onClick={handleClose}>
-            <IoClose size={23} color="white"/>
-          </div>
-          <img
-            src="ecosystembg.png"
-            alt="Background"
-            className="absolute inset-0 w-full h-full object-cover opacity-30"
-          />
-          <div className="relative z-10 text-white font-semibold text-2xl text-center">
-            {selectedEcosystem || "No Ecosystem Selected"}
-          </div>
-
-          <div
-            className="relative z-50 text-sm font-medium bg-white mx-auto px-4 py-2 cursor-pointer rounded-md shadow-md"
-            onClick={handleEcosystem} 
-          >
-            Explore Usecases
-          </div>
+    <div className="flex flex-col gap-4 items-center bg-white shadow-lg  rounded overflow-y-auto scrollbar-thin m-auto w-[400px] max-h-[85vh]">
+      <div className="relative flex flex-col gap-8 py-6 px-4 w-full bg-blue-400">
+        <div
+          className="text-white font-semibold absolute right-1 top-1 cursor-pointer z-10"
+          onClick={handleClose}
+        >
+          <IoClose size={23} color="white" />
+        </div>
+        <img
+          src="ecosystembg.png"
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover opacity-50 z-0"
+        />
+        <div className="relative z-10 text-center text-white font-medium text-xl">
+          {selectedEcosystem || "No Ecosystem Selected"}
         </div>
 
-        <div className="flex flex-col overflow-y-scroll h-[340px] scrollbar-thin">
-          {startups && startups.length > 0 ? (
-            startups.map((startup, index) => (
-              <div
-                key={index}
-                className="flex flex-col mx-2 px-2 py-1.5 mb-10 bg-white rounded-md shadow-md"
-              >
-                <div className="flex flex-row">
-                  <div className="flex flex-col gap-2">
-                    <div className="font-medium">{startup.name}</div>
-                    <div>{startup.description}</div>
-                    <div className="flex flex-row justify-center gap-2 items-center rounded-md p-1.5 border-2 border-black w-max cursor-pointer" onClick={handleExploreClick}>
-                      <div>Explore</div>
-                      <div>
-                        <FaGlobe />
-                      </div>
+        <div
+          className="relative z-50 text-sm font-medium bg-white mx-auto px-4 py-2 cursor-pointer rounded-md shadow-md"
+          onClick={handleEcosystem}
+        >
+          Explore Usecases
+        </div>
+      </div>
+
+      <div className="flex flex-col overflow-y-scroll scrollbar-thin">
+        {startups && startups.length > 0 ? (
+          startups.map((startup, index) => (
+            <div
+              key={index}
+              className="flex flex-col mx-2 px-2  py-1.5 mb-10  rounded-md shadow-sm shadow-gray-400 border-gray-400"
+            >
+              <div className="flex flex-row ">
+                <div className="flex flex-col gap-2">
+                  <div className="font-medium">{startup.name}</div>
+                  <div>{startup.description}</div>
+                  <div
+                    className="flex flex-row justify-center gap-2 items-center rounded-md py-1 px-2.5 border-2 border-sky-500 w-max cursor-pointer text-sky-500 mt-1 mb-1.5"
+                    onClick={handleExploreClick}
+                  >
+                    <div>Explore</div>
+                    <div>
+                      <FaGlobe />
                     </div>
                   </div>
-                  <div className="flex justify-center items-center">
-                    <img
-                      src={startup.imgSrc || "default-image.png"}
-                      alt={startup.name}
-                      className="w-40 object-cover h-[80px]"
-                    />
-                  </div>
+                </div>
+                <div className="flex justify-center items-center">
+                  <img
+                    src={startup.imgSrc || "default-image.png"}
+                    alt={startup.name}
+                    className="w-40 object-cover h-[80px]"
+                  />
                 </div>
               </div>
-            ))
-          ) : (
-            <div className="text-gray-500">No startups available.</div>
-          )}
-        </div>
+            </div>
+          ))
+        ) : (
+          <div className="text-gray-500">No startups available.</div>
+        )}
       </div>
     </div>
   );
