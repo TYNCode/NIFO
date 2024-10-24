66e2ac2b68e9ad1c19fdb2e4c63122a00144c069
lathiesh24 <lathumahi24@gmail.com>
2024-10-24 16:54:03 +0530

    RECENT

--- src/app/components/TrendsWeb/NavbarTrend.tsx
+++ src/app/components/TrendsWeb/NavbarTrend.tsx
@@ -3,13 +3,19 @@ import { useRouter } from 'next/navigation';
 
 const NavbarTrend = () => {
   const router = useRouter();
+
+  const handleRoute = ()=>{
+    router.push("/")
+  }
+
   return (
     <div className=' w-full z-50'>
       <div className='shadow-md'>
         <img
         src="/nifoimage.png"
         alt="Nifo Logo"
-        className='w-24'/>
+        className='w-24 cursor-pointer'
+        onClick={handleRoute}/>
       </div>
     </div>
   )
