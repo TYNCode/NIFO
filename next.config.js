1309f2724d9e2e112b909e3869f7eba011603f50
RakeshMahendran <rakeshmahendran99@example.com>
2025-04-23 09:41:16 +0530

    changes in the home page

--- next.config.js
+++ next.config.js
@@ -2,20 +2,40 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", 
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
   env: {
-    PUBLIC_BASE_URL_LOCAL: process.env.NEXT_PUBLIC_BASE_URL_LOCAL
+    PUBLIC_BASE_URL_LOCAL: process.env.NEXT_PUBLIC_BASE_URL_LOCAL,
   },
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "media.licdn.com",
-      "encrypted-tbn0.gstatic.com",
-      "www.wofsummit.com",
-      "lh3.googleusercontent.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wofsummit.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
     ],
   },
 });
