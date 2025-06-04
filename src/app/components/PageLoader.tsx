cacd623be91adf3d161b2fec12d109d97f699a5f
Code-TheYellowNetwork <innovation@theyellow.network>
2025-06-04 12:10:59 +0530

    Refactor chat session page

---
+++ src/app/components/PageLoader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BounceLoading from "./HomePage/BounceLoading/BounceLoading";
+
+const PageLoader = () => (
+  <div className="flex justify-center mt-10">
+    <BounceLoading />
+  </div>
+);
+
+export default PageLoader;
